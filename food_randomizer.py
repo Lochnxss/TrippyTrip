a54bc9dfc05937ee63bf453622d504ee7eeb9a05
Lochnxss <Zuess90@outlook.com>
2025-05-11 23:07:42 -0400

    Update food_randomizer.py

--- food_randomizer.py
+++ food_randomizer.py
@@ -17,7 +17,7 @@ from datetime import datetime
 from hashlib import sha256
 
 st.set_page_config(page_title="The Grazing Trail", layout="wide")
-st.write"apploaded"
+st.write("apploaded")
                    
 # Semi-transparent forest backdrop
 st.markdown("""
